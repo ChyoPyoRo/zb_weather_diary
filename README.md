40de764a95fc6d3aff9652312388d8a96b4500c7
ChyoPyoRo <allen15590@naver.com>
2025-03-17 20:06:21 +0900

    Swagger 적용

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Zero-base Weather Diary Project
+
+날씨 정보와 일기를 저장하는 서버
+
+사용 스택 : Swagger, SpringBoot, Spring Data Jpa
+
+
+
+![img.png](img.png)
--- src/main/java/com/zerobase/weather/controller/DiaryController.java
+++ src/main/java/com/zerobase/weather/controller/DiaryController.java
@@ -8,6 +8,8 @@ import com.zerobase.weather.enums.Description;
 import com.zerobase.weather.exception.CustomException;
 import com.zerobase.weather.service.DiaryDetailService;
 import com.zerobase.weather.utils.DateValidator;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -15,12 +17,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@Tag(name = "Diary Api", description = "일기 관련 APi")
 public class DiaryController {
     @Autowired
     private DiaryDetailService diaryDetailService;
 
 
     @PostMapping(value = "/create/diary")
+    @Operation(summary = "일기 생성", description = "캐싱되어 있는 날씨 정보를 기반으로 일기 생성")
     public ResponseEntity<ResponseDto> createDiary(@RequestBody CreateDiaryReqsDto requestDto) {
         if(!DateValidator.isValidDate(requestDto.getDate())) {throw new CustomException(CustomeException.INVALID_REQUEST);}
         ResponseDto response = diaryDetailService.createDiary(requestDto);
@@ -28,6 +32,7 @@ public class DiaryController {
     }
 
     @GetMapping(value = "/read/diary")
+    @Operation(summary = "일기 불러오기", description = "하나의 일기만 불러옴")
     public ResponseEntity<ResponseDto> readDiary(@RequestParam String date) {
         if(date == null || !DateValidator.isValidDate(date)) {throw new CustomException(CustomeException.INVALID_REQUEST);}
         ResponseDto response = diaryDetailService.findDiaryOnDate(date);
@@ -35,6 +40,7 @@ public class DiaryController {
     }
 
     @GetMapping(value="/read/diaries")
+    @Operation(summary="기간 내 일기 불러오기", description="기간 내의 일기를 전부 불러옴")
     public ResponseEntity<ResponseDto> readDiaries(@RequestParam String startDate, @RequestParam String endDate) {
         if(startDate == null || !DateValidator.isValidDate(startDate)) {throw new CustomException(CustomeException.INVALID_REQUEST);}
         if(endDate == null || !DateValidator.isValidDate(endDate)) {throw new CustomException(CustomeException.INVALID_REQUEST);}
@@ -43,6 +49,7 @@ public class DiaryController {
     }
 
     @PutMapping(value = "/update/diary")
+    @Operation(summary="일기 수정하기", description = "해당 날짜의 일기들 중 첫번째 일기 수정")
     public ResponseEntity<ResponseDto> updateDiary(@RequestBody UpdateDairyReqDto updateDairyReqDto){
         if(!DateValidator.isValidDate(updateDairyReqDto.getDate())) {throw new CustomException(CustomeException.INVALID_REQUEST);}
         ResponseDto response = diaryDetailService.updateDiary(updateDairyReqDto);
@@ -50,6 +57,7 @@ public class DiaryController {
     }
 
     @DeleteMapping(value="/delete/diary")
+    @Operation(summary="일기 삭제하기", description = "해당 날짜의 모든 일기 삭제")
     public ResponseEntity<ResponseDto> deleteDiary(@RequestParam String date){
         if(!DateValidator.isValidDate(date)) {throw new CustomException(CustomeException.INVALID_REQUEST);}
         ResponseDto response = diaryDetailService.deleteDiary(date);
